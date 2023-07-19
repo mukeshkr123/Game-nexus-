28cfdbf50b862eaae75049f2b23e773b3b907c2d
mukesh <mukeshmehta2041@gmail.com>
2023-07-19 11:42:32 +0530

    Creating a responsive layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+        templateColumns={{
+          base: "1fr",
+          lg: "200px 1fr",
+        }}
+      >
+        <GridItem area="nav">Navbar </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" padding="5px">
+            Genrelists{" "}
+          </GridItem>
+        </Show>
+        <GridItem area="main">Games</GridItem>
+      </Grid>
+    </>
+  );
 };
 
 export default App;
