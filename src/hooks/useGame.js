d7359b56f3b9d6f2ff0f56ff12edf7de7a5ed043
mukesh <mukeshmehta2041@gmail.com>
2023-07-23 18:10:24 +0530

    bug2

--- src/hooks/useGame.js
+++ src/hooks/useGame.js
@@ -1,18 +1,23 @@
+// src/hooks/useData.js
+// Let's assume useData.js is in the correct location with the appropriate implementation.
+
+// src/hooks/useGames.js
 import useData from "./useData";
 
-const useGames = (gameQuery) =>
-  useData(
-    "/games",
-    {
-      params: {
-        genres: gameQuery?.genre?.id,
-        platforms: gameQuery?.platform?.id,
-        ordering: gameQuery.sortOrder,
-        search: gameQuery.searchText,
-      },
-    },
+const useGames = (gameQuery) => {
+  const params = {
+    genres: gameQuery?.genre?.id,
+    platforms: gameQuery?.platform?.id,
+    ordering: gameQuery.sortOrder,
+    search: gameQuery.searchText,
+  };
 
-    [gameQuery]
-  );
+  const gameData = useData("/games", params, [gameQuery]);
+
+  // You can also add additional logic or side-effects here if needed
+  // using the gameData fetched from the useData hook.
+
+  return gameData;
+};
 
 export default useGames;
